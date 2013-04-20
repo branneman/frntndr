3e716dad568075e00c1efd9ada68ad5647fc4c75
Bran van der Meer <branmovic@gmail.com>
2013-04-20 17:05:36 +0200

    On build, the views directory is not copied anymore, since the httpcopy already puts the files in the build.

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,7 +28,7 @@ module.exports = function(grunt) {
                 files: [{
                     expand: true,
                     cwd: 'src/',
-                    src: ['**'],
+                    src: ['**', '!**/views/**'],
                     dest: 'build'
                 }]
             }
