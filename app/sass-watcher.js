5aa0a9af9b4ae0ce47b2863534862a0a411f3ff8
Bran van der Meer <branmovic@gmail.com>
2013-11-29 14:44:29 +0100

    Grunt watch errors are now logged to console

--- app/sass-watcher.js
+++ app/sass-watcher.js
@@ -4,8 +4,10 @@ var exec = require('child_process').exec;
 
 module.exports = function() {
 
-    // Start grunt watcher (which in turn starts sass watcher, csslint and autoprefixer)
-    var sassProcess = exec('grunt watcher', function(err, stdout, stderr) {});
+    // Start grunt watcher (which in turn starts sass watcher and autoprefixer)
+    var sassProcess = exec('grunt watcher', function(err, stdout, stderr) {
+        console.log(err, stdout, stderr);
+    });
 
     // Exit watcher when node.js is getting killed
     process.on('SIGINT', function() {
