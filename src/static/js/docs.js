7648111fd1f8202902c6e7c52ab07f1b402492c1
Bran van der Meer <branmovic@gmail.com>
2013-12-06 13:01:04 +0100

    Documentation: the Resizable Demo module now centers the iframe whenever possible

--- src/static/js/docs.js
+++ src/static/js/docs.js
@@ -18,10 +18,17 @@ $(function ResizableDemo() {
 
     // Resize demo
     $('.controls .active button:not(.close)', $resizableDemo).on('click', function() {
-        $resizable.css({
+        var styles = {
+            left: '',
+            'margin-left': '',
             width: $(this).attr('data-width'),
             height: $(this).attr('data-height')
-        });
+        };
+        if (styles.width !== 'auto') {
+            styles.left = '50%';
+            styles['margin-left'] = -(parseInt(styles.width, 10) / 2);
+        }
+        $resizable.css(styles);
     });
 
 }());
