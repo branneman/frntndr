373bd43ce3e14b0e1b9fb7e7c330591c21a4887d
branneman <branmovic@gmail.com>
2014-05-25 13:30:53 +0200

    Rewrote docs.js, now vanilla javascript.

--- src/static/js/docs.js
+++ src/static/js/docs.js
@@ -1,60 +1,76 @@
 //
-// Quick Reference Card
+// Module: Quick Reference Card
 //
-$(function QuickReferenceCard(){
+(function QuickReferenceCard() {
 
     'use strict';
 
-    $('iframe').load(function() {
-        $(this).css({
-            height: $(this.contentDocument.body).outerHeight() + 10
+    document.addEventListener('DOMContentLoaded', function() {
+
+        var iframes = document.querySelectorAll('iframe');
+        if (!iframes.length) return;
+
+        Array.prototype.forEach.call(iframes, function(iframe) {
+
+            iframe.contentWindow.addEventListener('load', (function(iframe) {
+                return function() {
+                    iframe.style.height = iframe.contentDocument.body.scrollHeight + 10 + 'px';
+                };
+            }(iframe)));
         });
     });
-});
+}());
 
 //
-// Resizable Demo module
+// Module: Resizable Demo
 //
-
-$(function ResizableDemo() {
+(function ResizableDemo() {
 
     'use strict';
 
-    var $resizableDemo = $('.resizable-demo');
-    var $resizable     = $('.resizable', $resizableDemo);
+    document.addEventListener('DOMContentLoaded', function(e) {
 
-    // Open Resizable Demo
-    $('.controls .open', $resizableDemo).on('click', function() {
-        resetStyles();
-        $resizableDemo.addClass('fullscreen');
-    });
+        if (e.target.defaultView.frameElement) return;
 
-    // Close Resizable Demo
-    $('.controls .close', $resizableDemo).on('click', function() {
-        resetStyles();
-        $resizableDemo.removeClass('fullscreen');
-    });
+        var resizableDemo = document.querySelector('.resizable-demo');
+        var resizable     = resizableDemo.querySelector('.resizable');
 
-    // Resize demo
-    $('.controls .active button:not(.close)', $resizableDemo).on('click', function() {
-        resetStyles();
-        var styles = {
-            width: $(this).attr('data-width'),
-            height: $(this).attr('data-height')
-        };
-        if (styles.width !== 'auto') {
-            styles.left = '50%';
-            styles['margin-left'] = -(parseInt(styles.width, 10) / 2);
+        function resetStyles() {
+            resizable.style.left = '';
+            resizable.style.marginLeft = '';
+            resizable.style.width = '';
+            resizable.style.height = '';
         }
-        $resizable.css(styles);
-    });
 
-    var resetStyles = function() {
-        $resizable.css({
-            left: '',
-            'margin-left': '',
-            width: '',
-            height: ''
+        // Event: Open dialog
+        resizableDemo.querySelector('.controls .open').addEventListener('click', function() {
+            resetStyles();
+            resizableDemo.classList.add('fullscreen');
         });
-    };
-});
+
+        // Event: Close dialog
+        resizableDemo.querySelector('.controls .close').addEventListener('click', function() {
+            resetStyles();
+            resizableDemo.classList.remove('fullscreen');
+        });
+
+        // Events: Resize buttons
+        var buttons = resizableDemo.querySelectorAll('.controls .active button:not(.close)');
+        Array.prototype.forEach.call(buttons, function(button) {
+
+            button.addEventListener('click', function(evt) {
+                resetStyles();
+
+                var width  = evt.target.getAttribute('data-width');
+                var height = evt.target.getAttribute('data-height');
+
+                if (width !== 'auto') {
+                    resizable.style.width      = width + 'px';
+                    resizable.style.height     = height + 'px';
+                    resizable.style.left       = '50%';
+                    resizable.style.marginLeft = -(parseInt(resizable.style.width, 10) / 2) + 'px';
+                }
+            });
+        });
+    });
+}());
